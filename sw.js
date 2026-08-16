2257467298b8dcef4fbeef64b16b81b9c6a73f8c
anshuji15g <anshuji15g@gmail.com>
2026-08-16 18:13:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,30 @@
-const CACHE_NAME = 'cartcreaterji-v1';
+const CACHE_NAME = 'cartcreaterji-v2';
 const ASSETS = [
   './index.html',
   './style.css',
   './app.js',
-  './manifest.json'
+  './manifest.json',
+  './icon.svg'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
 });
 
